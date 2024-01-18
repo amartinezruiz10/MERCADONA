db1ee33adeb5a56c4904328a06863446bafc848d
Alejandro <a.martinezruiz@globant.com>
2024-01-18 13:33:07 +0100

    cuartoCommit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {listStyleType: {
-        'square': 'square',
-      },},
-    },
+    content: ["./public/**/*.{html,js}"],
+    
     plugins: [],
   }
